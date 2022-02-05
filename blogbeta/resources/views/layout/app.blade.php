6c54dd47e9ca9ee3829ab30e43f7a29484e1654b
hugoresende27 <Hugoresende27@gmail.com>
2022-02-05 09:02:30 +0000

    a

--- blogbeta/resources/views/layout/app.blade.php
+++ blogbeta/resources/views/layout/app.blade.php
@@ -18,6 +18,7 @@
         rel="stylesheet"
         href="https://use.fontawesome.com/releases/v5.0.7/css/all.css"
     />
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 
 <body>
